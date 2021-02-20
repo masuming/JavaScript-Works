8ef6e83f26eb5492360a94895b6d07e7f4e1a016
masuming <mail@masuming.ivory.ne.jp>
2021-02-20 16:36:56 +0900

    ルーレットのJSファイルを新規作成

---
+++ roulette/js/roulette.js
@@ -0,0 +1,80 @@
+$(function(){
+    
+    let roulette=$("#roulette__img");
+    let mawasu=$("#mawasu");
+    let rClear=$("#rclear");
+    let result=$("#result");
+    let rRotate;
+    let flag=false;
+    
+    $(mawasu).css("opacity",1).css("filter","grayscale(0)");
+    $(rClear).css("opacity",0.5).css("filter","grayscale(0.9)").css("cursor","auto");
+    $(rClear).on("mouseover",function(){
+        $(rClear).css({transform: "scale(0.9)"});
+    });
+
+    $(mawasu).on("click",function(){
+
+        $(mawasu).css({transform: "scale(0.9)"});
+        $(mawasu).stop().css("opacity",0.5).css("filter","grayscale(0.9)").css("cursor","auto");
+
+        if(flag==false){    
+            rRotate=Math.random();
+            rRotate=Math.floor(rRotate*10+1)*36+720;
+            $(roulette).css({transition: "3s"});
+            $(roulette).css({transform: "rotate("+rRotate+"deg)"});
+
+            setTimeout(function(){
+                $(result).css("display","block");
+                if(rRotate<=756){
+                $(result).css("background-image","url(images/go_02.png)");
+                } else if(rRotate<=792){
+                    $(result).css("background-image","url(images/go_01.png)");
+                } else if(rRotate<=828){
+                    $(result).css("background-image","url(images/go_10.png)");
+                } else if(rRotate<=864){
+                    $(result).css("background-image","url(images/go_09.png)");
+                } else if(rRotate<=900){
+                    $(result).css("background-image","url(images/go_08.png)");
+                } else if(rRotate<=936){
+                    $(result).css("background-image","url(images/go_07.png)");
+                } else if(rRotate<=972){
+                    $(result).css("background-image","url(images/go_06.png)");
+                } else if(rRotate<=1008){
+                    $(result).css("background-image","url(images/go_05.png)");
+                } else if(rRotate<=1044){
+                    $(result).css("background-image","url(images/go_04.png)");
+                } else {
+                    $(result).css("background-image","url(images/go_03.png)");
+                }
+            },4000);
+     
+            setTimeout(function(){
+                $(result).css("display","none");  
+                flag=true;
+                $(rClear).css("opacity",1).css("filter","grayscale(0)").css("cursor","pointer");
+                $(rClear).on("mouseover",function(){
+                    $(rClear).css({transform: "scale(1)"});
+                });
+                $(rClear).on("mouseout",function(){
+                    $(rClear).css({transform: "scale(0.9)"});
+                });
+            },6500);
+        }
+    });
+    
+    $(rClear).click(function(){
+        if(flag==true){
+            location.reload();
+            $(rClear).on("mouseover",function(){
+                $(rClear).css({transform: "scale(0.9)"});
+            });
+            $(rClear).on("mouseout",function(){
+                $(rClear).css({transform: "scale(0.9)"});
+            });
+            flag==false;
+        }
+    });
+
+
+})
